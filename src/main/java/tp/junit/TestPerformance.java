97d2793ccdcc14c9db58e4752b120d78dd7f373d
isma3l <karlo.ismael@gmail.com>
2013-11-20 04:01:22 -0300

    la clase testperformance fue corregido, ahora funciona el tiempo :)

--- src/main/java/tp/junit/TestPerformance.java
+++ src/main/java/tp/junit/TestPerformance.java
@@ -12,17 +12,39 @@ public class TestPerformance extends TestDecorator{
         this.endtime=0;
     }
 
-    @Override
-    public void runTest() {
-        endtime= System.nanoTime();
+    public void verifiedTimeLimit() {
         long elapsedTime=(endtime-starttime)/ 1000000;
         if (elapsedTime>timelimit) throw new AssertException();
     }
 
     @Override
     public void run(TestResult result) {
+        testCase.setAttribute(setUp);
+        if (testCase.isRunner()) {
+            try {
+                runTest();
+                verifiedTimeLimit();
+                result.addPassed(testCase.getName(), calcularTiempo(starttime, endtime));
+            } catch (AssertException e) {
+                endtime = System.nanoTime();
+                result.addFail(testCase.getName(), calcularTiempo(starttime, endtime));
+            } catch (Throwable e) {
+                endtime = System.nanoTime();
+                result.addError(testCase.getName(), calcularTiempo(starttime, endtime));
+            }
+        } else {
+            result.addSkipped(testCase.getName(), 0);
+        }
+    }
+
+    @Override
+    public void runTest() {
         starttime = System.nanoTime();
-        testCase.run(result);
-        runTest();
+        testCase.runTest();
+        endtime = System.nanoTime();
+    }
+
+    private double calcularTiempo(double tiempoInicial, double tiempoFinal) {
+        return (tiempoFinal - tiempoInicial) / 1000000;
     }
 }
