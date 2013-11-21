6da8b74e64b03ac8e08ab6ed3c0a911aae34bcf5
luis <aliluis@gmail.com>
2013-11-21 02:08:23 -0300

    commite de la interfaz comun para store y una implementacion vacia de store Version 1.1

--- src/main/java/tp/junit/Store.java
+++ src/main/java/tp/junit/Store.java
@@ -1,5 +1,8 @@
 package tp.junit;
 
+import java.util.List;
+
 public interface Store {
    public void addTestToBlackList(String testname);
+   public List<String> getBlackList();
 }
---
+++ src/main/java/tp/junit/XmlStore.java
@@ -0,0 +1,9 @@
+package tp.junit;
+
+
+public class XmlStore implements Store{
+    @Override
+    public void addTestToBlackList(String testname) {
+
+    }
+}
