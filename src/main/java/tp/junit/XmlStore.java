85296ca130800fec756ea4c0ff3d326c37964d2c
luis <aliluis@gmail.com>
2013-11-21 16:01:17 -0300

    XmlStore Terminado V 2

--- src/main/java/tp/junit/XmlStore.java
+++ src/main/java/tp/junit/XmlStore.java
@@ -4,6 +4,8 @@ package tp.junit;
 import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -14,63 +16,71 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class XmlStore implements Store{
-    Document doc;
-    Element root;
-
+    private DocumentBuilder docBuilder;
+    private Document doc;
+    private Element root;
+    private static final String filename="Store.xml";
+    private static final String Blacklist="Blacklist";
+    private static final String Testcase="testcase";
+    private static final String Attname="name";
     public XmlStore() {
         try {
             DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-            doc = docBuilder.newDocument();
-            root = doc.createElement("OKTest");
-            doc.appendChild(root);
+            docBuilder = docFactory.newDocumentBuilder();
         } catch (ParserConfigurationException e) {
 
         }
 
     }
     private void agregarTest(Element blacklistelem, String TestCaseName) {
-        Element nameTestCase = doc.createElement("testcase");
-        Attr nameTC = doc.createAttribute("name");
-
+        Element nameTestCase = doc.createElement(XmlStore.Testcase);
+        Attr nameTC = doc.createAttribute(XmlStore.Attname);
         nameTC.setValue(TestCaseName);
-
         nameTestCase.setAttributeNode(nameTC);
-
-        Attr nameElementTC = doc.createAttribute("name");
+        Attr nameElementTC = doc.createAttribute(XmlStore.Attname);
         nameElementTC.setValue(TestCaseName);
-
         blacklistelem.appendChild(nameTestCase);
 
     }
-    public void guardar(String nameFile) {
+    public void guardar() {
         try {
             Transformer transformer = TransformerFactory.newInstance().newTransformer();
             DOMSource source = new DOMSource(doc);
-            Result result = new StreamResult(new java.io.File(nameFile + ".xml"));
+            Result result = new StreamResult(new java.io.File(XmlStore.filename));
             transformer.transform(source, result);
         } catch (TransformerException tfe) {
         }
     }
     @Override
     public void addTestToBlackList(ArrayList<TestState> blacklist) {
-        Element blacklistelem = doc.createElement("Blacklist");
-        Attr name = doc.createAttribute("name");
-        name.isId();
-        name.setValue("Blacklist");
-        blacklistelem.setAttributeNode(name);
-        root.appendChild(blacklistelem);
-
-        for (TestState state : blacklist) {
-                agregarTest(blacklistelem, state.getTestCaseName());
-        }
+        doc = docBuilder.newDocument();
+        root = doc.createElement(XmlStore.Blacklist);
+        doc.appendChild(root);
+        for (TestState state : blacklist)
+            agregarTest(root, state.getTestCaseName());
+        guardar();
     }
 
     @Override
     public ArrayList<TestState> getBlackList() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        ArrayList<TestState> blacklist= new ArrayList<TestState>();
+        try {
+            doc=docBuilder.parse(new File(XmlStore.filename));
+            NodeList nodeList = doc.getElementsByTagName(XmlStore.Blacklist);
+            for(int i=0; i<nodeList.getLength(); i++) {
+                String testname=nodeList.item(i).getAttributes().getNamedItem(XmlStore.Attname).getNodeValue();
+                blacklist.add(new TestState(testname, TestState.State.PASSED, 0));
+            }
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return blacklist;
     }
 }
---
+++ src/test/java/casosDePrueba/CasoRecordarTetConFallidosStoreXml.java
@@ -0,0 +1,24 @@
+package casosDePrueba;
+
+import aplicacion.TestCalculadora;
+import aplicacion.TestError;
+import aplicacion.TestRestaCalculadora;
+import tp.junit.*;
+
+import java.io.IOException;
+
+public class CasoRecordarTetConFallidosStoreXml {
+    public static void main(String[] args) throws IOException {
+        TestRunnerStore handler = new TestRunnerStore(new TestRunner());
+        TestSuite suite = new TestSuite("suite");
+        TestCalculadora testCalculadora = new TestCalculadora();
+        TestError testError = new TestError();
+        TestRestaCalculadora testRestaCalculadora = new TestRestaCalculadora();
+
+        suite.addTest(testError);
+        suite.addTest(testRestaCalculadora);
+        suite.addTest(testCalculadora);
+        handler.setStore(new XmlStore());
+        handler.run(suite);
+    }
+}
