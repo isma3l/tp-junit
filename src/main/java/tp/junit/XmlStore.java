32c65efb8edd6bf93a4b0b11339348811c454a4e
luis <aliluis@gmail.com>
2013-11-21 02:37:06 -0300

    una implementacion vacia de store Version 1.2

--- src/main/java/tp/junit/XmlStore.java
+++ src/main/java/tp/junit/XmlStore.java
@@ -1,9 +1,16 @@
 package tp.junit;
 
 
+import java.util.List;
+
 public class XmlStore implements Store{
     @Override
     public void addTestToBlackList(String testname) {
 
+    }
+
+    @Override
+    public List<String> getBlackList() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 }
